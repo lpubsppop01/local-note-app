73bf7722eca0078159265675577d3a59b13ac29f
lpubsppop01 <lpubsppop01@gmail.com>
2018-06-07 20:46:52 +0900

    Fix issue that month number display is one less

--- src/common/date-utility.ts
+++ src/common/date-utility.ts
@@ -3,7 +3,7 @@ export default class DateUtility {
   static formatElispLike(format: string, date: Date) {
     return format
       .replace(/%Y/g, this.zeroPadding(date.getFullYear(), 4))
-      .replace(/%m/g, this.zeroPadding(date.getMonth(), 2))
+      .replace(/%m/g, this.zeroPadding(date.getMonth() + 1, 2))
       .replace(/%d/g, this.zeroPadding(date.getDate(), 2))
       .replace(/%H/g, this.zeroPadding(date.getHours(), 2))
       .replace(/%M/g, this.zeroPadding(date.getMinutes(), 2))
